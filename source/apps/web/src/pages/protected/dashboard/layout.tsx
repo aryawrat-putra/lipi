b41c6d6c9bf34af287e17087d7aa60737c825329
Saitama <contact.yourwebdevgmail.com>
2025-11-29 18:57:47 +0530

    basic toolboor and menu ui in editor page

--- source/apps/web/src/pages/protected/dashboard/layout.tsx
+++ source/apps/web/src/pages/protected/dashboard/layout.tsx
@@ -1,24 +1,10 @@
 import { SidebarProvider, SidebarTrigger, SidebarInset } from "@repo/ui/components/sidebar";
-import { Link, Outlet } from "react-router";
+import { Outlet } from "react-router";
 import { AppSidebar } from "../../../components/sidebar/app-sidebar";
 import { Separator } from "@repo/ui/components/separator";
-import { Avatar, AvatarImage, AvatarFallback } from "@repo/ui/components/avatar";
-import { BookOpen, DollarSign, ExternalLink, HelpCircle, LogOut, Settings, User, Users } from "lucide-react";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuGroup,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@repo/ui/components/dropdown-menu";
-import { Tooltip, TooltipContent, TooltipTrigger } from "@repo/ui/components/tooltip";
-import { Button } from "@repo/ui/components/button";
-
-import { ModeToggle } from "../../../components/theme-toggle";
 import Logo from "../../../components/logo";
 import FileSearch from "../../../components/forms/file-search";
+import UserMenu from "../../../components/user-menu";
 
 export default function DashboardLayout() {
     return (
@@ -39,97 +25,7 @@ export default function DashboardLayout() {
                         <span className="lg:min-w-sm max-sm:hidden"><FileSearch /></span>
 
                         {/* User Menu */}
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                                <Button variant="outline" size={'icon'}>
-                                    <Tooltip>
-                                        <TooltipTrigger asChild>
-                                            <Avatar>
-                                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                                                <AvatarFallback>JD</AvatarFallback>
-                                            </Avatar>
-                                        </TooltipTrigger>
-                                        <TooltipContent className="space-y-1">
-                                            <p>John Doe</p>
-                                            <p>mail@johndoe.com</p>
-                                        </TooltipContent>
-                                    </Tooltip>
-                                </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent className="w-3xs space-y-2" align="end">
-                                <div className="px-2 py-3 border-b border-border">
-                                    <p className="font-semibold text-sm">John Doe</p>
-                                    <p className="text-xs text-muted-foreground truncate">doe@john.com</p>
-                                </div>
-
-                                <DropdownMenuGroup>
-                                    <DropdownMenuLabel>Account</DropdownMenuLabel>
-                                    <DropdownMenuItem className="cursor-pointer" asChild>
-                                        <Link to={'/settings/profile'}>
-                                            <User className="mr-2 h-4 w-4" />
-                                            <span>Profile</span>
-                                        </Link>
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem className="cursor-pointer" asChild>
-                                        <Link to={'/settings'}>
-                                            <Settings className="mr-2 h-4 w-4" />
-                                            <span>Settings</span>
-                                        </Link>
-                                    </DropdownMenuItem>
-                                </DropdownMenuGroup>
-
-                                <DropdownMenuSeparator />
-
-                                <DropdownMenuGroup>
-                                    <DropdownMenuLabel>Other</DropdownMenuLabel>
-                                    <DropdownMenuItem className="cursor-pointer" asChild>
-                                        <Link to='#'>
-                                            <DollarSign className="mr-2 h-4 w-4" />
-                                            <span>Pricing</span>
-                                            <ExternalLink className="ml-auto h-3 w-3 opacity-50" />
-                                        </Link>
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem className="cursor-pointer" asChild>
-                                        <Link to='#'>
-                                            <BookOpen className="mr-2 h-4 w-4" />
-                                            <span>Documentation</span>
-                                            <ExternalLink className="ml-auto h-3 w-3 opacity-50" />
-                                        </Link>
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem className="cursor-pointer" asChild>
-                                        <Link to='#'>
-                                            <Users className="mr-2 h-4 w-4" />
-                                            <span>Community Forum</span>
-                                            <ExternalLink className="ml-auto h-3 w-3 opacity-50" />
-                                        </Link>
-                                    </DropdownMenuItem>
-                                    <DropdownMenuItem className="cursor-pointer" asChild>
-                                        <Link to='#'>
-                                            <HelpCircle className="mr-2 h-4 w-4" />
-                                            <span>Feedback</span>
-                                            <ExternalLink className="ml-auto h-3 w-3 opacity-50" />
-                                        </Link>
-                                    </DropdownMenuItem>
-                                </DropdownMenuGroup>
-
-                                <DropdownMenuSeparator />
-
-                                <DropdownMenuGroup>
-                                    <DropdownMenuLabel>Preferences</DropdownMenuLabel>
-                                    <div className="px-2 py-1.5"><ModeToggle /></div>
-                                </DropdownMenuGroup>
-
-                                <DropdownMenuSeparator />
-
-                                {/* Sign Out */}
-                                <DropdownMenuItem className="cursor-pointer text-destructive focus:text-destructive" asChild>
-                                    <Link to={'/logout'}>
-                                        <LogOut className="mr-2 h-4 w-4" />
-                                        <span>Sign Out</span>
-                                    </Link>
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
+                        <UserMenu />
                     </header>
 
                     {/* Page Starts */}
--- source/apps/web/src/pages/protected/document/index.tsx
+++ source/apps/web/src/pages/protected/document/index.tsx
@@ -1,12 +1,95 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import { Link, useParams } from 'react-router'
+import Logo from '../../../components/logo';
+import { Button } from '@repo/ui/components/button';
+import { CloudCheck, FileClock, FileCog, FilePenLine, MessagesSquare, SquareStar } from 'lucide-react';
+import UserMenu from '../../../components/user-menu';
+import DocMenubar from '../../../components/document/doc-menubar';
+import { Input } from '@repo/ui/components/input';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@repo/ui/components/tooltip';
+import DocEditor from '../../../components/document/doc-editor';
 
-type Props = {}
+import { Collapsible, CollapsibleContent} from "@repo/ui/components/collapsible"
+import { useState } from "react"
 
-export default function DocumentPage({}: Props) {
-   let params = useParams();
+export default function DocumentPage() {
+  let { docId } = useParams();
+  const [isToolbarOpen, setIsToolbarOpen] = useState<boolean>(true);
 
   return (
-    <div>Document Page : '{params.docId}' </div>
+    <section className='container p-2 mx-auto'>
+      <header className='sticky top-0 left-0 backdrop-blur-lg'>
+        <Collapsible
+          open={isToolbarOpen}
+          onOpenChange={setIsToolbarOpen}
+        >
+          <CollapsibleContent>
+            <div className='flex justify-between items-center gap-2'>
+              <nav className='flex justify-between items-center gap-4 '>
+                <Link to='/'><Logo /></Link>
+                <div className='space-y-2'>
+                  <div className='flex gap-2 items-center'>
+                    <Input
+                      type="text"
+                      placeholder="Document Name"
+                      defaultValue={docId}
+                      className='read-only:bg-muted max-w-xs w-fit'
+                      readOnly
+                    />
+
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button variant='ghost' size='icon'><FilePenLine /></Button>
+                      </TooltipTrigger>
+                      <TooltipContent><p>Edit Name</p></TooltipContent>
+                    </Tooltip>
+
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button variant='ghost' size='icon'><SquareStar /></Button>
+                      </TooltipTrigger>
+                      <TooltipContent><p>Favorite Document</p></TooltipContent>
+                    </Tooltip>
+
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <Button variant='ghost' size='icon'><CloudCheck /></Button>
+                      </TooltipTrigger>
+                      <TooltipContent><p>Sync Document</p></TooltipContent>
+                    </Tooltip>
+                  </div>
+                  <DocMenubar />
+                </div>
+              </nav>
+              <div className='flex items-center gap-4'>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button variant='secondary' size='icon'><FileClock /></Button>
+                  </TooltipTrigger>
+                  <TooltipContent><p>History</p></TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button variant='secondary' size='icon'><MessagesSquare /></Button>
+                  </TooltipTrigger>
+                  <TooltipContent><p>Comments</p></TooltipContent>
+                </Tooltip>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button variant='secondary' size='icon'><FileCog /></Button>
+                  </TooltipTrigger>
+                  <TooltipContent><p>Document Preferences</p></TooltipContent>
+                </Tooltip>
+                <UserMenu />
+              </div>
+            </div>
+          </CollapsibleContent>
+          <aside className='my-2'>
+            <DocEditor />
+          </aside>
+        </Collapsible>
+
+      </header>
+      <main className='min-h-screen'>page</main>
+    </section>
   )
 }
